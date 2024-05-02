1b581d763894ff3b3efeafdfc283df9129877470
Tommy <mckeont@upenn.edu>
2024-05-02 00:36:37 -0400

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -6,6 +6,6 @@ var map = L.map('map', {
   maxZoom: 19            // Optional: maximum zoom level
 });
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '© OpenStreetMap contributors'
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+  attribution: 'Tiles © Esri — Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
 }).addTo(map);
